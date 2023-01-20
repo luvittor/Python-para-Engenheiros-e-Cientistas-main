4e098294190be88cbbb5ecd8eae27deccb4d7aef
luvittor <luciano.vittoretti@gmail.com>
2023-01-20 16:05:05 -0300

    curso

---
+++ Notebooks/untitled0.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 20 15:37:23 2023
+
+@author: luvit
+"""
+
+a = 1
+b = [x for x in range(100)]
+
+
+
+c=[]
+
+for i in range(100):
+    c.append(i**2)
+    
+    
+    
